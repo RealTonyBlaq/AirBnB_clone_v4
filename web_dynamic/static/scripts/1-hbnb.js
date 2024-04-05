fe8d6e9d30d0417c655d9d92321e22007a34ea52
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-05 21:35:46 +0100

    script

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-    let amenityIds = [];
+  let amenityIds = [];
 
-    $('input[type="checkbox"]').change(function () {
-      const amenityId = $(this).data('id');
-  
-      if ($(this).prop('checked')) {
-        amenityIds.push(amenityId);
-      } else {
-        amenityIds = amenityIds.filter(id => id !== amenityId);
-      }
-      updateDisplay();
-    });
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
 
-    function updateDisplay () {
-      const amenitiesText = amenityIds.length > 0 ? amenityIds.join(', ') : '&nbsp;';
-      $('.popover h4').html(amenitiesText);
+    if ($(this).prop('checked')) {
+      amenityIds.push(amenityId);
+    } else {
+      amenityIds = amenityIds.filter(id => id !== amenityId);
     }
+    updateDisplay();
+  });
+
+  function updateDisplay () {
+    const amenitiesText = amenityIds.length > 0 ? amenityIds.join(', ') : '&nbsp;';
+    $('.popover h4').html(amenitiesText);
+  }
 });
