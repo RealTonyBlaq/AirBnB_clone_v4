c3ff02c458fdb4edfce931caeb5fb2b780784aa5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-04-05 21:31:24 +0100

    script

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-  $('input').listen()
-});
+    // Initialize an empty array to store Amenity IDs
+    let amenityIds = [];
+  
+    // Listen for changes on each input checkbox tag
+    $('input[type="checkbox"]').change(function () {
+      // Retrieve Amenity ID from data-id attribute
+      const amenityId = $(this).data('id');
+  
+      if ($(this).prop('checked')) {
+        amenityIds.push(amenityId);
+      } else {
+        // If unchecked, remove the Amenity ID from the array
+        amenityIds = amenityIds.filter(id => id !== amenityId);
+      }
+      // Update the h4 tag inside the div Amenities with the list of Amenities checked
+      updateAmenitiesDisplay();
+    });
+  
+    // Function to update the h4 tag with the list of Amenities checked
+    function updateAmenitiesDisplay () {
+      const amenitiesText = amenityIds.length > 0 ? amenityIds.join(', ') : '&nbsp;';
+      $('.popover h4').html(amenitiesText);
+    }
+  });
